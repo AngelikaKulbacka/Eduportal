f80af5af83fd33cb8f1f38500d3e1c377deb34b3
Angelika Kulbacka <angipimpek@gmail.com>
2022-05-11 18:12:08 +0200

    baza

--- baza/isam.sql
+++ baza/isam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 07 Maj 2019, 17:50
+-- Czas generowania: 12 Cze 2019, 00:19
 -- Wersja serwera: 10.1.34-MariaDB
 -- Wersja PHP: 7.2.7
 
@@ -44,6 +44,79 @@ INSERT INTO `administratorzy` (`id_administratora`, `login`, `haslo`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `lekcja`
+--
+
+CREATE TABLE `lekcja` (
+  `id_lekcji` int(11) NOT NULL,
+  `id_moderatora` int(11) NOT NULL,
+  `tytul` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `tresc` text COLLATE utf8_polish_ci NOT NULL,
+  `styl` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `lekcja`
+--
+
+INSERT INTO `lekcja` (`id_lekcji`, `id_moderatora`, `tytul`, `tresc`, `styl`) VALUES
+(42, 1, 'Procesor', 'Tutaj dowiesz siÄ™ o architekturze procesora.', 2),
+(43, 1, 'Procesor', 'Tutaj dowiesz siÄ™ wszystkiego o architekturze procesora. Zobaczysz takĹĽe jak poprawnie zamontowaÄ‡ go w komputerze.', 5),
+(44, 1, 'PamiÄ™Ä‡ RAM', 'W tej lekcji dowiesz siÄ™ jak dziaĹ‚ajÄ… oraz jak sÄ… zbudowane pamiÄ™ci RAM. Ponadto poznasz odmiany pamiÄ™ci jak i starsze oraz nowsze modele RAMu.', 2),
+(45, 1, 'PamiÄ™Ä‡ RAM', 'W tej lekcji dowiesz siÄ™ jak dziaĹ‚a pamiÄ™Ä‡ ram oraz jak umieĹ›ciÄ‡ jÄ… w komputerze.', 2),
+(46, 1, 'Karta graficzna', 'W tej lekcji dowiesz siÄ™ jak dziaĹ‚a karta graficzna.  Dowiesz siÄ™ takĹĽe jak prawidĹ‚owo osadziÄ‡ jÄ… w komputerze.', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `lekcjalink`
+--
+
+CREATE TABLE `lekcjalink` (
+  `id_materialu` int(11) NOT NULL,
+  `id_lekcji` int(11) NOT NULL,
+  `id_linku` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `lekcjalink`
+--
+
+INSERT INTO `lekcjalink` (`id_materialu`, `id_lekcji`, `id_linku`) VALUES
+(30, 42, 34),
+(31, 43, 35),
+(32, 44, 36),
+(33, 45, 37),
+(34, 46, 38),
+(35, 46, 39);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `linki`
+--
+
+CREATE TABLE `linki` (
+  `id_linku` int(11) NOT NULL,
+  `tytul` varchar(100) COLLATE utf8_polish_ci NOT NULL,
+  `link` varchar(100) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `linki`
+--
+
+INSERT INTO `linki` (`id_linku`, `tytul`, `link`) VALUES
+(34, 'Procesor PDF', '4735procek.pdf'),
+(35, 'MontaĹĽ procesora:', '5542film procesor i radiator.mp4'),
+(36, 'RAM PDF:', '2310pamieci.pdf'),
+(37, 'Umieszczanie pamiÄ™ci RAM', '7112film ram.mp4'),
+(38, 'Karta graficzna PDF', '535grafa.pdf'),
+(39, 'Karta graficzna film instruktaĹĽowy:', '4281film graficzna.mp4');
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `moderatorzy`
 --
 
@@ -59,7 +132,7 @@ CREATE TABLE `moderatorzy` (
 
 INSERT INTO `moderatorzy` (`id_moderatora`, `login`, `haslo`) VALUES
 (1, 'moderator', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'),
-(2, 'mod2', 'e55cffc81a5ad8cfe85239d944a3ae9513645a9eed79bc884f51b80b2760fc46');
+(4, 'moderator2', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
 
 -- --------------------------------------------------------
 
@@ -68,45 +141,86 @@ INSERT INTO `moderatorzy` (`id_moderatora`, `login`, `haslo`) VALUES
 --
 
 CREATE TABLE `odpowiedz` (
-  `id_odpowiedzi` int(11) NOT NULL,
+  `id_odpowiedzi` int(25) NOT NULL,
   `odpowiedz` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `czy_poprawna` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
+--
+-- Zrzut danych tabeli `odpowiedz`
+--
+
+INSERT INTO `odpowiedz` (`id_odpowiedzi`, `odpowiedz`, `czy_poprawna`) VALUES
+(17, 'blok ALU', 0),
+(18, 'koder liczb zmiennopozycyjnych', 1),
+(19, 'ukĹ‚ad sterujÄ…cy', 0),
+(20, 'pastÄ™ do butĂłw ', 0),
+(21, 'pasztet', 0),
+(22, 'pastÄ™ termoprzewodzÄ…cÄ…', 1),
+(23, 'PamiÄ™Ä‡ dynamiczna (DIMM)', 1),
+(24, 'PamiÄ™Ä‡ statyczna', 0),
+(25, 'PamiÄ™Ä‡ flash', 0),
+(26, '2', 0),
+(27, '4', 0),
+(28, '8', 1),
+(29, 'VGA', 0),
+(30, 'S-Video', 0),
+(31, 'HDMI', 1);
+
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `pytanie_odpowiedzi`
+-- Struktura tabeli dla tabeli `pytaniekontrolne`
 --
 
-CREATE TABLE `pytanie_odpowiedzi` (
-  `id_pyt_odp` int(11) NOT NULL,
-  `id_pytania` int(11) NOT NULL,
+CREATE TABLE `pytaniekontrolne` (
+  `id_pyt_odp` int(25) NOT NULL,
+  `id_lekcji` int(11) NOT NULL,
   `id_odpowiedzi` int(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
+--
+-- Zrzut danych tabeli `pytaniekontrolne`
+--
+
+INSERT INTO `pytaniekontrolne` (`id_pyt_odp`, `id_lekcji`, `id_odpowiedzi`) VALUES
+(16, 42, 17),
+(17, 42, 18),
+(18, 42, 19),
+(19, 43, 20),
+(20, 43, 21),
+(21, 43, 22),
+(22, 44, 23),
+(23, 44, 24),
+(24, 44, 25),
+(25, 45, 26),
+(26, 45, 27),
+(27, 45, 28),
+(28, 46, 29),
+(29, 46, 30),
+(30, 46, 31);
+
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `pytanie_tresc`
+-- Struktura tabeli dla tabeli `pytanietresc`
 --
 
-CREATE TABLE `pytanie_tresc` (
-  `id_pytania` int(11) NOT NULL,
+CREATE TABLE `pytanietresc` (
+  `id_lekcji` int(11) NOT NULL,
   `tresc` varchar(250) COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
--- --------------------------------------------------------
-
 --
--- Struktura tabeli dla tabeli `quiz`
+-- Zrzut danych tabeli `pytanietresc`
 --
 
-CREATE TABLE `quiz` (
-  `id_quizu` int(11) NOT NULL,
-  `id_lekcji` int(11) NOT NULL,
-  `id_pytania` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+INSERT INTO `pytanietresc` (`id_lekcji`, `tresc`) VALUES
+(42, 'Jednym z podstawowych elementĂłw procesora NIE jest?'),
+(43, 'Co umieszczamy na procesorze?'),
+(44, 'Jakim typem pamiÄ™ci jest pamiÄ™Ä‡ SDRAM?'),
+(45, 'Ile miejsc (SLOTĂ“W) zawiera zaprezentowana na filmie pĹ‚yta gĹ‚Ăłwna?'),
+(46, 'Najpopularniejszym obecnie wyjĹ›ciem karty graficznej, mogÄ…cym przesĹ‚aÄ‡ sygnaĹ‚ w jakoĹ›ci 4K jest?');
 
 -- --------------------------------------------------------
 
@@ -115,7 +229,7 @@ CREATE TABLE `quiz` (
 --
 
 CREATE TABLE `style` (
-  `id_stylu` int(11) NOT NULL,
+  `styl` int(11) NOT NULL,
   `nazwa` varchar(50) COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
@@ -123,11 +237,12 @@ CREATE TABLE `style` (
 -- Zrzut danych tabeli `style`
 --
 
-INSERT INTO `style` (`id_stylu`, `nazwa`) VALUES
+INSERT INTO `style` (`styl`, `nazwa`) VALUES
 (1, 'brak'),
-(2, 'słuchowiec'),
-(3, 'wzrokowiec'),
-(4, 'kinestetyk');
+(2, 'przyswajanie-wzrokowiec'),
+(3, 'przyswajanie-sĹ‚uchowiec'),
+(4, 'dziaĹ‚anie-wzrokowiec'),
+(5, 'dziaĹ‚anie-sĹ‚uchowiec');
 
 -- --------------------------------------------------------
 
@@ -148,10 +263,14 @@ CREATE TABLE `uzytkownicy` (
 --
 
 INSERT INTO `uzytkownicy` (`id_uzytkownika`, `login`, `haslo`, `email`, `styl`) VALUES
-(1, 'user', '04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb', 'user@gmail.com', 0);
+(1, 'user', '04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb', 'user@gmail.com', 3),
+(2, 'user2', '6025d18fe48abd45168528f18a82e265dd98d421a7084aa09f61b341703901a3', 'user2@gmail.com', 2),
+(3, 'user3', '5860faf02b6bc6222ba5aca523560f0e364ccd8b67bee486fe8bf7c01d492ccb', 'user3@gmail.com', 2),
+(4, 'user4', '5269ef980de47819ba3d14340f4665262c41e933dc92c1a27dd5d01b047ac80e', 'user4@gmail.com', 4),
+(5, 'user5', '5a39bead318f306939acb1d016647be2e38c6501c58367fdb3e9f52542aa2442', 'user5@gmail.com', 3);
 
 --
--- Indeksy dla zrzutów tabel
+-- Indeksy dla zrzutĂłw tabel
 --
 
 --
@@ -160,6 +279,28 @@ INSERT INTO `uzytkownicy` (`id_uzytkownika`, `login`, `haslo`, `email`, `styl`) 
 ALTER TABLE `administratorzy`
   ADD PRIMARY KEY (`id_administratora`),
   ADD UNIQUE KEY `login_admin` (`login`);
+
+--
+-- Indeksy dla tabeli `lekcja`
+--
+ALTER TABLE `lekcja`
+  ADD PRIMARY KEY (`id_lekcji`),
+  ADD KEY `styl` (`styl`),
+  ADD KEY `id_moderatora` (`id_moderatora`);
+
+--
+-- Indeksy dla tabeli `lekcjalink`
+--
+ALTER TABLE `lekcjalink`
+  ADD PRIMARY KEY (`id_materialu`),
+  ADD KEY `id_lekcji` (`id_lekcji`),
+  ADD KEY `id_linku` (`id_linku`);
+
+--
+-- Indeksy dla tabeli `linki`
+--
+ALTER TABLE `linki`
+  ADD PRIMARY KEY (`id_linku`);
 
 --
 -- Indeksy dla tabeli `moderatorzy`
@@ -175,28 +316,24 @@ ALTER TABLE `odpowiedz`
   ADD PRIMARY KEY (`id_odpowiedzi`);
 
 --
--- Indeksy dla tabeli `pytanie_odpowiedzi`
+-- Indeksy dla tabeli `pytaniekontrolne`
 --
-ALTER TABLE `pytanie_odpowiedzi`
-  ADD PRIMARY KEY (`id_pyt_odp`);
+ALTER TABLE `pytaniekontrolne`
+  ADD PRIMARY KEY (`id_pyt_odp`),
+  ADD KEY `id_odpowiedzi` (`id_odpowiedzi`),
+  ADD KEY `id_lekcji` (`id_lekcji`);
 
 --
--- Indeksy dla tabeli `pytanie_tresc`
+-- Indeksy dla tabeli `pytanietresc`
 --
-ALTER TABLE `pytanie_tresc`
-  ADD PRIMARY KEY (`id_pytania`);
-
---
--- Indeksy dla tabeli `quiz`
---
-ALTER TABLE `quiz`
-  ADD PRIMARY KEY (`id_quizu`);
+ALTER TABLE `pytanietresc`
+  ADD PRIMARY KEY (`id_lekcji`);
 
 --
 -- Indeksy dla tabeli `style`
 --
 ALTER TABLE `style`
-  ADD PRIMARY KEY (`id_stylu`);
+  ADD PRIMARY KEY (`styl`);
 
 --
 -- Indeksy dla tabeli `uzytkownicy`
@@ -204,7 +341,8 @@ ALTER TABLE `style`
 ALTER TABLE `uzytkownicy`
   ADD PRIMARY KEY (`id_uzytkownika`),
   ADD UNIQUE KEY `login_user` (`login`),
-  ADD UNIQUE KEY `email_user` (`email`);
+  ADD UNIQUE KEY `email_user` (`email`),
+  ADD KEY `styl` (`styl`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -217,40 +355,94 @@ ALTER TABLE `administratorzy`
   MODIFY `id_administratora` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT dla tabeli `lekcja`
+--
+ALTER TABLE `lekcja`
+  MODIFY `id_lekcji` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT dla tabeli `lekcjalink`
+--
+ALTER TABLE `lekcjalink`
+  MODIFY `id_materialu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT dla tabeli `linki`
+--
+ALTER TABLE `linki`
+  MODIFY `id_linku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
 -- AUTO_INCREMENT dla tabeli `moderatorzy`
 --
 ALTER TABLE `moderatorzy`
-  MODIFY `id_moderatora` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_moderatora` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT dla tabeli `pytanie_odpowiedzi`
+-- AUTO_INCREMENT dla tabeli `odpowiedz`
 --
-ALTER TABLE `pytanie_odpowiedzi`
-  MODIFY `id_pyt_odp` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `odpowiedz`
+  MODIFY `id_odpowiedzi` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
--- AUTO_INCREMENT dla tabeli `pytanie_tresc`
+-- AUTO_INCREMENT dla tabeli `pytaniekontrolne`
 --
-ALTER TABLE `pytanie_tresc`
-  MODIFY `id_pytania` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `pytaniekontrolne`
+  MODIFY `id_pyt_odp` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
--- AUTO_INCREMENT dla tabeli `quiz`
+-- AUTO_INCREMENT dla tabeli `pytanietresc`
 --
-ALTER TABLE `quiz`
-  MODIFY `id_quizu` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `pytanietresc`
+  MODIFY `id_lekcji` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT dla tabeli `style`
 --
 ALTER TABLE `style`
-  MODIFY `id_stylu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `styl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT dla tabeli `uzytkownicy`
 --
 ALTER TABLE `uzytkownicy`
-  MODIFY `id_uzytkownika` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_uzytkownika` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Ograniczenia dla zrzutĂłw tabel
+--
+
+--
+-- Ograniczenia dla tabeli `lekcja`
+--
+ALTER TABLE `lekcja`
+  ADD CONSTRAINT `lekcja_ibfk_1` FOREIGN KEY (`styl`) REFERENCES `style` (`styl`);
+
+--
+-- Ograniczenia dla tabeli `lekcjalink`
+--
+ALTER TABLE `lekcjalink`
+  ADD CONSTRAINT `lekcjalink_ibfk_1` FOREIGN KEY (`id_linku`) REFERENCES `linki` (`id_linku`),
+  ADD CONSTRAINT `lekcjalink_ibfk_2` FOREIGN KEY (`id_lekcji`) REFERENCES `lekcja` (`id_lekcji`);
+
+--
+-- Ograniczenia dla tabeli `pytaniekontrolne`
+--
+ALTER TABLE `pytaniekontrolne`
+  ADD CONSTRAINT `pytaniekontrolne_ibfk_3` FOREIGN KEY (`id_lekcji`) REFERENCES `lekcja` (`id_lekcji`),
+  ADD CONSTRAINT `pytaniekontrolne_ibfk_4` FOREIGN KEY (`id_odpowiedzi`) REFERENCES `odpowiedz` (`id_odpowiedzi`);
+
+--
+-- Ograniczenia dla tabeli `pytanietresc`
+--
+ALTER TABLE `pytanietresc`
+  ADD CONSTRAINT `pytanietresc_ibfk_1` FOREIGN KEY (`id_lekcji`) REFERENCES `lekcja` (`id_lekcji`);
+
+--
+-- Ograniczenia dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  ADD CONSTRAINT `uzytkownicy_ibfk_2` FOREIGN KEY (`styl`) REFERENCES `style` (`styl`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
